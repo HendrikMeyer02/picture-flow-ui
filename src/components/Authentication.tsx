dfabbffd36415e9973f5e247c5d60b193cf4b724
HendrikMeyer <hendrik.meyer@sap.com>
2024-02-17 13:39:17 +0200

    Update Authentication.tsx
    you can create new users, but exception handling is not possible

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -1,224 +1,233 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
 import "./authentication.css";
 import Footer from "./footer";
-import { Link } from "react-router-dom";
 import Header from "./misc/Header";
-import { useReducer, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Cookies from "universal-cookie";
-import jwt from "jwt-decode";
-
 
 export default function Authentication() {
-    //Global constants
-    const [loginState, setLogin] = useState(true);
-    const [buttonText, setButtonText] = useState("Anmelden");
-    const navigate = useNavigate();
-    const cookies = new Cookies();
+  //Global constants
+  const [loginState, setLogin] = useState(true);
+  const [buttonText, setButtonText] = useState("Anmelden");
+  const navigate = useNavigate();
+  const cookies = new Cookies();
 
-    //Login Validation
+  //Login Validation
 
-    //flips elements for registration
-    function flipElements() {
-        const switchText = document.getElementById("signupTrigger");
-        const gridItems = document.querySelectorAll(".grid-item");
-        const usernameInput = document.getElementById(
-            "username-input"
-        ) as HTMLInputElement;
-        const forgotPassword = document.getElementById(
-            "forgot-password"
-        ) as HTMLInputElement;
+  //flips elements for registration
+  function flipElements() {
+    const switchText = document.getElementById("signupTrigger");
+    const gridItems = document.querySelectorAll(".grid-item");
+    const usernameInput = document.getElementById(
+      "username-input"
+    ) as HTMLInputElement;
+    const forgotPassword = document.getElementById(
+      "forgot-password"
+    ) as HTMLInputElement;
 
-        usernameInput.value = "";
+    usernameInput.value = "";
 
-        if (loginState == true) {
-            //Wenn man zur Registrierung geht
+    if (loginState == true) {
+      //Wenn man zur Registrierung geht
 
-            setButtonText("Registrieren");
-            switchText!.innerHTML = "Vorhandenes Konto";
-            forgotPassword.style.display = "block";
-            forgotPassword.innerHTML = "Registrierung nur als Demo";
+      setButtonText("Registrieren");
+      switchText!.innerHTML = "Vorhandenes Konto";
+      forgotPassword.style.display = "block";
+      forgotPassword.innerHTML = "Registrierung nur als Demo";
 
-            gridItems.forEach((item) => {
-                item.classList.toggle("hidden");
-            });
+      gridItems.forEach((item) => {
+        item.classList.toggle("hidden");
+      });
 
-            gridItems.forEach((item) => {
-                item.classList.toggle("unhidden");
-            });
+      gridItems.forEach((item) => {
+        item.classList.toggle("unhidden");
+      });
 
-            setLogin(false);
-        } else {
-            //Wenn man zum Login geht
+      setLogin(false);
+    } else {
+      //Wenn man zum Login geht
 
-            setButtonText("Anmelden");
-            switchText!.innerHTML = "Kein Konto";
-            forgotPassword.style.display = "none";
-            forgotPassword.innerHTML = "Falsche Anmeldedaten!";
+      setButtonText("Anmelden");
+      switchText!.innerHTML = "Kein Konto";
+      forgotPassword.style.display = "none";
+      forgotPassword.innerHTML = "Falsche Anmeldedaten!";
 
-            gridItems.forEach((item) => {
-                item.classList.toggle("hidden");
-            });
+      gridItems.forEach((item) => {
+        item.classList.toggle("hidden");
+      });
 
-            gridItems.forEach((item) => {
-                item.classList.toggle("unhidden");
-            });
+      gridItems.forEach((item) => {
+        item.classList.toggle("unhidden");
+      });
 
-            setLogin(true);
-        }
+      setLogin(true);
     }
+  }
 
-    //Events
-    const [values, setValues] = useState({
-        email: "",
-        password: "",
+  //Events
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    userName: "",
+  });
+
+  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    const forgotPassword = document.getElementById(
+      "forgot-password"
+    ) as HTMLInputElement;
+    e.preventDefault();
+
+    //Überprüft die eingegebenen Daten.
+    //Kann erweitert werden mit vorhandenem Backend
+    if (loginState == true) {
+      const token = await fetch("http://localhost:8000/auth/login", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          email: values["email"],
+          password: values["password"],
+        }),
+      });
+      const authToken = await token.json();
+      if (token.statusText == "OK") {
+        login(authToken.AuthToken);
+        navigate("/main");
+      } else {
+        forgotPassword.style.display = "block";
+      }
+    } else {
+      const token = await fetch("http://localhost:8000/auth/register", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          email: values["email"],
+          password: values["password"],
+          username: values["userName"],
+        }),
+      });
+      const authToken = await token.json();
+      if (token.statusText == "OK") {
+        login(authToken.AuthToken);
+        navigate("/main");
+      } else {
+        console.log(token.statusText);
+        forgotPassword.style.display = "block";
+      }
+    }
+  };
+
+  //Überprüft die Eingaben
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const emailErr = document.getElementById("email-err");
+
+    setValues({ ...values, [e.target.name]: e.target.value });
+
+    const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (e.target.name == "email") {
+      if (emailReg.test(e.target.value)) {
+        emailErr!.style.display = "none";
+      } else {
+        emailErr!.style.display = "block";
+      }
+    }
+  };
+
+  const login = (token) => {
+    const today = new Date();
+
+    cookies.set("authorization", token, {
+      expires: new Date(today.getTime() + 3600000 * 24 * 14),
     });
+  };
 
-    const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
-        const forgotPassword = document.getElementById(
-            "forgot-password"
-        ) as HTMLInputElement;
-        e.preventDefault();
-        //Email Data
-        const data = new FormData(e.target);
-        const formDataObject = Object.fromEntries(data.entries());
-        const email = formDataObject.email.toString();
-        const password = formDataObject.password.toString();
-
-        //Überprüft die eingegebenen Daten.
-        //Kann erweitert werden mit vorhandenem Backend
-        if (loginState == true) {
-            console.log(values["email"] + values["password"])
-            const token = await fetch("http://localhost:8000/auth/login", {
-                method: "POST",
-                mode: "cors",
-                body: JSON.stringify({
-                    "email": values["email"],
-                    "password": values["password"]
-                })
-            });
-            const authToken = await token.json();
-            console.log(token.statusText);
-            if (token.statusText == "OK") {
-                login(authToken.AuthToken);
-                navigate("/main");
-            } else {
-                forgotPassword.style.display = "block";
-            }
-        }
-    };
-
-    //Überprüft die Eingaben
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const emailErr = document.getElementById("email-err");
-
-        setValues({ ...values, [e.target.name]: e.target.value });
-
-        const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        console.log(e.target.name);
-        if (e.target.name == "email") {
-            if (emailReg.test(e.target.value)) {
-                emailErr!.style.display = "none";
-            } else {
-                emailErr!.style.display = "block";
-            }
-        }
-    };
-
-    const login = (token) => {
-        const today = new Date();
-
-        cookies.set("authorization", token, {
-            expires: new Date(today.getTime() + 3600000 * 24 * 14),
-        })
-    }
-
-
-    return (
-        <>
-            <div className="authentication-container">
-                <form
-                    className="glass"
-                    id="authentication-form"
-                    onSubmit={handleSubmit}
-                >
-                    <div id="header">
-                        <div id="authentication-back-arrow-container">
-                            <Link id="authentication-back-arrow-link" to="/">
-                                <img
-                                    id="authentication-back-arrow"
-                                    src="./img/icon/back-arrow.png"
-                                ></img>
-                            </Link>
-                        </div>
-
-                        <Header></Header>
-                        <img id="authentication-header-image" src="img/logo.png"></img>
-                    </div>
-                    <div id="error"></div>
-
-                    <div className="form grid-item hidden" id="username-form">
-                        <input
-                            id="username-input"
-                            type="text"
-                            name="username"
-                            autoComplete="off"
-                        />
-                        <label htmlFor="username" className="label-name">
-                            <span className="content-name">Nutzername</span>
-                        </label>
-                    </div>
-                    <p className="error" id="username-err"></p>
-
-                    <div className="form" id="email-form">
-                        <input
-                            onChange={onChange}
-                            value={values["email"]}
-                            id="email-input"
-                            type="text"
-                            name="email"
-                            autoComplete="off"
-                            required
-                        />
-                        <label htmlFor="email" className="label-name">
-                            <span className="content-name">Email</span>
-                        </label>
-                    </div>
-                    <p className="error" id="email-err">
-                        Ungültige E-Mail Adresse.
-                    </p>
-
-                    <div className="form" id="password-form">
-                        <input
-                            onChange={onChange}
-                            value={values["password"]}
-                            id="password-input"
-                            type="password"
-                            name="password"
-                            autoComplete="off"
-                            required
-                        />
-                        <label htmlFor="password" className="label-name">
-                            <span className="content-name">Passwort</span>
-                        </label>
-                    </div>
-                    <p className="error" id="password-err">
-                        Falsche Anmeldedaten!
-                    </p>
-
-                    <p id="forgot-password">Falsche Anmeldedaten.</p>
-                    <input
-                        type="submit"
-                        autoComplete="off"
-                        id="authentication-button"
-                        value={buttonText}
-                        className="square-button"
-                    ></input>
-                    <p id="signupTrigger" onClick={flipElements}>
-                        Neues Konto
-                    </p>
-                </form>
-                <Footer />
+  return (
+    <>
+      <div className="authentication-container">
+        <form
+          className="glass"
+          id="authentication-form"
+          onSubmit={handleSubmit}
+        >
+          <div id="header">
+            <div id="authentication-back-arrow-container">
+              <Link id="authentication-back-arrow-link" to="/">
+                <img
+                  id="authentication-back-arrow"
+                  src="./img/icon/back-arrow.png"
+                ></img>
+              </Link>
             </div>
-        </>
-    );
+
+            <Header></Header>
+            <img id="authentication-header-image" src="img/logo.png"></img>
+          </div>
+          <div id="error"></div>
+
+          <div className="form grid-item hidden" id="username-form">
+            <input
+              id="username-input"
+              onChange={onChange}
+              value={values["userName"]}
+              type="text"
+              name="userName"
+              autoComplete="off"
+            />
+            <label htmlFor="username" className="label-name">
+              <span className="content-name">Nutzername</span>
+            </label>
+          </div>
+          <p className="error" id="username-err"></p>
+
+          <div className="form" id="email-form">
+            <input
+              onChange={onChange}
+              value={values["email"]}
+              id="email-input"
+              type="text"
+              name="email"
+              autoComplete="off"
+              required
+            />
+            <label htmlFor="email" className="label-name">
+              <span className="content-name">Email</span>
+            </label>
+          </div>
+          <p className="error" id="email-err">
+            Ungültige E-Mail Adresse.
+          </p>
+
+          <div className="form" id="password-form">
+            <input
+              onChange={onChange}
+              value={values["password"]}
+              id="password-input"
+              type="password"
+              name="password"
+              autoComplete="off"
+              required
+            />
+            <label htmlFor="password" className="label-name">
+              <span className="content-name">Passwort</span>
+            </label>
+          </div>
+          <p className="error" id="password-err">
+            Falsche Anmeldedaten!
+          </p>
+
+          <p id="forgot-password">Falsche Anmeldedaten.</p>
+          <input
+            type="submit"
+            autoComplete="off"
+            id="authentication-button"
+            value={buttonText}
+            className="square-button"
+          ></input>
+          <p id="signupTrigger" onClick={flipElements}>
+            Neues Konto
+          </p>
+        </form>
+        <Footer />
+      </div>
+    </>
+  );
 }
